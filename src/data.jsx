59c25456265ea021ac165d7e41e6413f0c941061
Krystian <texen24@gmail.com>
2024-04-30 21:53:09 +0200

    prepared my project, done with RWD

---
+++ src/data.jsx
@@ -0,0 +1,44 @@
+export const listOfCharges = [
+  {
+    name: 'Czynsz Pabianicka',
+  },
+  {
+    name: 'Prąd Pabianicka',
+  },
+  {
+    name: 'Czynsz Piotrkowska',
+  },
+  {
+    name: 'Prąd Piotrkowska',
+  },
+  {
+    name: 'Gaz',
+  },
+  {
+    name: 'Toya Piotrkkowska',
+  },
+  {
+    name: 'Toya Pabianicka',
+  },
+  {
+    name: 'iPhone',
+  },
+  {
+    name: 'Netflix & telefon',
+  },
+];
+
+export const months = [
+  'Styczeń',
+  'Luty',
+  'Marzec',
+  'Kwiecień',
+  'Maj',
+  'Czerwiec',
+  'Lipiec',
+  'Sierpień',
+  'Wrzesień',
+  'Październik',
+  'Listopad',
+  'Grudzień',
+];
